35fb3a30dcd3a5791036ad584575bd66ad2ba6b7
adipso-tstoklosa <stoklosa@adipso.com>
2025-02-08 17:48:41 +0100

    initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const menuOpener = document.querySelector('.js-menu-mobile-opener');
+  const menuCloser = document.querySelector('.js-menu-mobile-closer');
+  const menuMobile = document.querySelector('#menu-mobile');
+
+  menuOpener.addEventListener('click', () => {
+    const menuMobileLink = document.querySelectorAll('.menu-mobile-link')
+    menuMobile.classList.add('open');
+    body.classList.add('menu-mobile-open');
+
+    menuMobileLink.forEach(link => link.addEventListener('click', () => {
+      menuMobile.classList.remove('open');
+      body.classList.remove('menu-mobile-open');
+    }))
+  });
+
+  menuCloser.addEventListener('click', () => {
+    menuMobile.classList.remove('open');
+    body.classList.remove('menu-mobile-open');
+  });
+
+});
